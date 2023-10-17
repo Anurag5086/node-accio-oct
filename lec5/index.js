78e63015309cf8df6e307429e0dc26e5f2e25dc1
Anurag Gupta <anuraggupta5086@gmail.com>
2023-10-17 22:50:20 +0530

    added lec 5

--- lec5/index.js
+++ lec5/index.js
@@ -1,10 +1,127 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Todo = require("./models/TodoSchema");
 require("dotenv").config();
 const app = express();
 
 app.use(express.json());
 const PORT = process.env.PORT;
+
+// POST - Create a Todo
+app.post("/todo", async (req, res) => {
+  const { title, isCompleted, username } = req.body;
+
+  if (title.length == 0 || isCompleted == null || username.length == 0) {
+    return res.status(400).send({
+      status: 400,
+      message: "Please enter the values in correct format!",
+    });
+  }
+
+  try {
+    const todoObj = new Todo({
+      title,
+      isCompleted,
+      username,
+      dateTime: new Date(),
+    });
+
+    await todoObj.save();
+
+    res.status(201).send({
+      status: 201,
+      message: "Todo created successfully!",
+    });
+  } catch (err) {
+    res.status(400).send({
+      status: 400,
+      message: "Todo creation failed!",
+      data: err,
+    });
+  }
+});
+
+// GET - Get all todos for a username
+app.get("/todos/:username", async (req, res) => {
+  const username = req.params.username;
+
+  try {
+    const todoList = await Todo.find({ username }).sort({ dateTime: 1 });
+
+    res.status(200).send({
+      status: 200,
+      message: "Fetched all todos for a username successfully!",
+      data: todoList,
+    });
+  } catch (err) {
+    res.status(400).send({
+      status: 400,
+      message: "Failed to fetch all todos for a username!",
+      data: err,
+    });
+  }
+});
+
+// GET - Get a single Todo
+app.get("/todo/:id", (req, res) => {
+  const todoId = req.params.id;
+
+  Todo.findById(todoId)
+    .then((todoData) => {
+      res.status(200).send({
+        status: 200,
+        message: "Fetched a todo successfully!",
+        data: todoData,
+      });
+    })
+    .catch((err) => {
+      res.status(400).send({
+        status: 400,
+        message: "Failed to fetch a single todo using id!",
+        data: err,
+      });
+    });
+});
+
+// DELETE - Delete a todo based on id
+app.delete("/todo/:id", async (req, res) => {
+  const todoId = req.params.id;
+
+  try {
+    await Todo.findByIdAndDelete(todoId);
+
+    res.status(200).send({
+      status: 200,
+      message: "Deleted a todo successfully!",
+    });
+  } catch (err) {
+    res.status(400).send({
+      status: 400,
+      message: "Failed to delete a todo based on id!",
+      data: err,
+    });
+  }
+});
+
+// PATCH - Update a todo
+app.patch("/todo", async (req, res) => {
+  const { id, title, isCompleted } = req.body;
+
+  try {
+    await Todo.findByIdAndUpdate(id, { title, isCompleted });
+
+    res.status(200).send({
+      status: 200,
+      message: "Updated a todo successfully!",
+    });
+  } catch (err) {
+    res.status(400).send({
+      status: 400,
+      message: "Failed to update a todo based on id!",
+      data: err,
+    });
+  }
+});
 
 mongoose
   .connect(process.env.MONGODB_URI)
